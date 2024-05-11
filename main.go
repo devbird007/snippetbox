3a3d168296eaed43c7c751fe8cce807923cdad5a
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-05-11 09:38:23 +0100

    Will work with customzing http headers

--- main.go
+++ main.go
@@ -26,8 +26,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet..."))
 }
 
-// Add a snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
