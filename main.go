9c5f75b2b476d708a0f81eb4507a484e74282787
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-05-12 00:35:01 +0100

    Add URL query strings functionality to snippetView

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice
@@ -23,7 +25,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetView handler function
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// Extract the value of the id parameter from the query
+	// string and try to convert it to an integer using the
+	// strconv.Atoi() function. If it can't be converted to
+	// an integer, or the value is less than 1, we return a 404
+	// page not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Use the fmt.Fprintf() function to interpolate the id value
+	// with our response and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display the specific snippet with ID %d...\n", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
